db1d0fec0c4434fd09c80730edf4fcc0dbb27aa2
sampath-a <sampathsringeri@gmail.com>
2019-08-19 20:23:24 +0530

    Added source code

---
+++ src/app/success/success.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+@Component({
+    selector:'app-success',
+    templateUrl:'./success.component.html',
+    styles:['h3{color:green; padding:20px; border:1px solid green}']
+    
+})
+export class SuccessComponent
+{
+
+}
